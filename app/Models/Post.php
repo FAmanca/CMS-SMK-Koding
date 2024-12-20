7f83eeef3b8b8ce00df692697959415a644e010f
FAmanca <fadlyamanca@gmail.com>
2024-12-20 15:00:55 +0700

    Setup Model &  Migration

---
+++ app/Models/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class Post extends Model
+{
+    protected $guarded = [];
+
+    public function category(): BelongsTo
+    {
+        return $this->BelongsTo(Category::class, 'category_id');
+    }
+    public function user(): BelongsTo
+    {
+        return $this->BelongsTo(User::class, 'user_id');
+    }
+}
